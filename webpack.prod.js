a1933b7e139139227bcc1f7218b9dbd2216ea943
Michiel Koning <michiel.koning@efocus.nl>
2018-04-09 15:25:41 +0200

    critical css

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,16 @@
+const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const BrotliPlugin = require('brotli-webpack-plugin');
+const HtmlCriticalPlugin = require("html-critical-webpack-plugin");
+
+const PATHS = {
+  src: path.resolve(__dirname, 'src'),
+  dist: path.resolve(__dirname, 'assets'),
+};
 
 module.exports = merge(common, {
   mode: 'production',
@@ -39,6 +46,13 @@ module.exports = merge(common, {
     }),
     new BrotliPlugin({
       test: /\.(js|css)$/,
+    }),
+    new HtmlCriticalPlugin({
+      src: 'https://adler.michielkoning.nl/',
+      dest: `${PATHS.dist}/css/critical.css`,
+      minify: true,
+      width: 1920,
+      height: 1080,
     }),
   ],
 });
