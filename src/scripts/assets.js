0b293b9ee222f86d785aa425bb3ce02bfcceed8a
Michiel Koning <michiel.koning@efocus.nl>
2018-04-10 10:40:01 +0200

    test

--- src/scripts/assets.js
+++ src/scripts/assets.js
@@ -1,4 +1,4 @@
 import 'sass/style.scss';
 
-require.context('favicons', true, /\.(svg|png|ico|xml|json|webmanifest)$/);
 require.context('icons', true, /\.svg$/);
+require.context('favicons', true, /\.(svg|png|ico|xml|json|webmanifest)$/);
